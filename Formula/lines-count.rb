2f5f2236cb63214b08e60cc660735dde8ee1077e
koheiyamayama <kohei.ccw.23@gmail.com>
2021-10-13 00:24:41 +0900

    url追加

--- Formula/lines-count.rb
+++ Formula/lines-count.rb
@@ -1,6 +1,7 @@
 class LinesCount < Formula
   desc "Counts the number of lines in any file or directory."
   homepage "https://github.com/koheiyamayama/lines-count"
+  url "https://github.com/koheiyamayama/lines-count/releases/download/v0.0.1/lines-count-0.0.1-x86_64-apple-darwin.tar.gz"
   sha256 "0a33d6fe156ad23936c6329022efcca58b6005fedf1f9ef444999e5f79aa87ad"
   version "0.0.1"
 
